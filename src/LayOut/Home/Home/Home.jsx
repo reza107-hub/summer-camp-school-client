d442aaac455479cde01ba062ad8d167b7783e41f
reza107-hub <rashidaminur104@gmail.com>
2023-06-07 19:54:36 +0600

    react helmet installed and title logo changed

--- src/LayOut/Home/Home/Home.jsx
+++ src/LayOut/Home/Home/Home.jsx
@@ -1,9 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
 const Home = () => {
-    return (
-        <div className="pt-14">
-            home
-        </div>
-    );
+  return (
+    <div className="pt-14">
+      <Helmet>
+        <title>CampSporty | Home</title>
+      </Helmet>
+      home
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import { Link, useRouteError } from "react-router-dom";
 import ErrorSettingimg from "./ErrorSettingimg";
+import { Helmet } from "react-helmet-async";
 
 const ErrorPage = () => {
   const { error } = useRouteError();
   return (
     <section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
+      <Helmet>
+        <title>CampSporty | Error Page</title>
+      </Helmet>
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <ErrorSettingimg />
 
