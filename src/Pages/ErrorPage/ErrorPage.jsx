f626a0e43551bfb9f89933774f4938c8b45be4bf
reza107-hub <rashidaminur104@gmail.com>
2023-06-07 19:38:12 +0600

    404 page done

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,24 @@
+import { Link, useRouteError } from "react-router-dom";
+import ErrorSettingimg from "./ErrorSettingimg";
+
+const ErrorPage = () => {
+  const { error } = useRouteError();
+  return (
+    <section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <ErrorSettingimg />
+
+        <div className="max-w-md text-center">
+          <p className="text-2xl font-semibold md:text-3xl mb-8">
+            {error?.message}
+          </p>
+          <Link to="/" className="btn btn-primary normal-case">
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../LayOut/Home/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
