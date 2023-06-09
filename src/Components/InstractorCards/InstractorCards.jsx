9fe67e080682e88d43c57f04946b6f72ee574e21
reza107-hub <rashidaminur104@gmail.com>
2023-06-09 07:27:11 +0600

    instructor json make done and fetch in ui

--- src/Components/InstractorCards/InstractorCards.jsx
+++ src/Components/InstractorCards/InstractorCards.jsx
@@ -1,14 +1,14 @@
 import "./InstractorCards.css";
-const InstructorCards = ({ image, name, email }) => {
+const InstructorCards = ({ instructor }) => {
   return (
     <div>
       <article className="card">
         <div className="temporary_text">
-          <img src={image} alt="" />
+          <img src={instructor?.image} alt="" />
         </div>
         <div className="card_content">
-          <span className="card_title">{name}</span>
-          <span className="card_subtitle">{email}</span>
+          <span className="card_title">{instructor?.name}</span>
+          <span className="card_subtitle">{instructor?.email}</span>
           <p className="card_description">
             Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
             dolor tempora aperiam itaque possimus at, cupiditate earum, quae
--- src/Pages/Home/PopularIntractor/PopularIntractor.jsx
+++ src/Pages/Home/PopularIntractor/PopularIntractor.jsx
@@ -1,20 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import InstructorCards from "../../../Components/InstractorCards/InstractorCards";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const PopularIntractor = () => {
-  
+  const { data: instructors = [] } = useQuery({
+    queryKey: ["instructors"],
+    queryFn: async () => {
+      const res = await fetch("instructor.json");
+      return res.json();
+    },
+  });
+
   return (
     <section className="container mx-auto">
       <SectionTitle
         heading={"Popular Instructor"}
         subHeading={"here's some popular instructor"}
       ></SectionTitle>
-      <div className="mt-16 flex flex-col justify-center items-center md:grid md:grid-cols-3 container mx-auto gap-10">
-        <InstructorCards
-          image={"https://i.ibb.co/tB4vrV3/istockphoto-1312940184-612x612.jpg"}
-          name={"abul malik"}
-          email={"emu@emu.com"}
-        />
+      <div className="mt-16 flex flex-col justify-center items-center md:grid md:grid-cols-3 container mx-auto gap-10 max-w-4xl">
+        {instructors.map((instructor) => (
+          <InstructorCards
+            key={instructor._id}
+            instructor={instructor}
+          ></InstructorCards>
+        ))}
       </div>
     </section>
   );
