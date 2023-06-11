e4055d409ec5839ac79328883cb49aa5a9ec383a
reza107-hub <rashidaminur104@gmail.com>
2023-06-11 23:00:14 +0600

    dashboard conditional done

--- src/LayOut/Dashboard.jsx
+++ src/LayOut/Dashboard.jsx
@@ -1,12 +1,14 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
+  const isInstructor = 0;
+  const isAdmin = 0;
+
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center space-y-8">
         {/* Page content here */}
-
         <label
           htmlFor="my-drawer-2"
           className="btn btn-primary btn-sm btn-outline drawer-button lg:hidden absolute left-0 top-0"
@@ -23,37 +25,79 @@ const Dashboard = () => {
             Dashboard
           </li>
           <div className="divider"></div>
-          <li>
-            <NavLink
-              end
-              className={({ isActive }) =>
-                isActive ? "text-accent font-bold" : ""
-              }
-              to={"/dashboard"}
-            >
-              Selected Class
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? "text-accent font-bold" : ""
-              }
-              to={"/dashboard/enrolledclasses"}
-            >
-              My Enrolled Classes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? "text-accent font-bold" : ""
-              }
-              to={"/dashboard/payment-history"}
-            >
-              Payment History
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "text-accent font-bold" : ""
+                  }
+                  to={"/dashboard/manageclasses"}
+                >
+                  Manage Classes
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? "text-accent font-bold" : ""
+                  }
+                  to={"/dashboard/manageusers"}
+                >
+                  Manage Users
+                </NavLink>
+              </li>
+            </>
+          ) : isInstructor ? (
+            <>
+              <li>
+                <NavLink
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "text-accent font-bold" : ""
+                  }
+                  to={"/dashboard/addclass"}
+                >
+                  Add a Class
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? "text-accent font-bold" : ""
+                  }
+                  to={"/dashboard/myclasses"}
+                >
+                  My Classes
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink
+                  end
+                  className={({ isActive }) =>
+                    isActive ? "text-accent font-bold" : ""
+                  }
+                  to={"/dashboard"}
+                >
+                  Dashboard Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? "text-accent font-bold" : ""
+                  }
+                  to={"/dashboard/enrolledclasses"}
+                >
+                  Enrolled Classes
+                </NavLink>
+              </li>
+            </>
+          )}
           <div className="divider"></div>
           <li>
             <Link to={"/"}>Home</Link>
